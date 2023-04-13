614759c797c5f592d320dbb84948448650f4389b
Taylor Watkins <99297085+uta-tsw0306@users.noreply.github.com>
2023-04-12 23:09:58 -0500

    Add home page, stylesheet & php include file

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+	include ('shared.inc.php');
+?>
+<!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="keywords" content="">
+            <meta name="description" content="">
+            <meta name="author" content="Web Wise Media">
+
+            <title>Home | Goodtimes Chorus</title>
+
+            <link rel="stylesheet" href="style.css">
+
+            <script src="https://kit.fontawesome.com/403c2f4f58.js" crossorigin="anonymous"></script>
+
+
+        </head>
+
+        <body>
+        <?php echo $nav; ?>
+
+            <main>
+                <div class="container-fluid">
+
+
+                
+                </div>
+            </main>
+
+            <?php echo $footer; ?>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+            <script src="js/app.js"></script>
+        </body>
+    </html>
---
+++ shared.inc.php
@@ -0,0 +1,105 @@
+<?php 
+$nav = "            
+    <header>
+        <nav>
+            <a href='index.php' class='navbar-brand'>Goodtimes Chorus</a>
+            <button class='navbar-toggler' type='button' data-target='#main-navigation'>
+                <i class='fa-solid fa-bars'></i>
+            </button>
+            <ul id='main-navigation' class='navbar'>
+                <li><a href='index.php' title='Home'>Home</a></li>
+                <li class='calendar-tab'><a href='calendar.php' title='Calendar'>Calendar</a>
+                    <ul class='calendar dropdown-content'>
+                        <li><a href='something.php' title='Title Goes Here'>Content 1</a></li>
+                        <li><a href='something.php' title='Title Goes Here'>Content 2</a></li>
+                    </ul>
+                </li>
+                <li class='about-tab'><a href='about.php' title='About'>About</a>
+                    <ul class='about dropdown-content'>
+                        <li><a href='something.php' title='Title Goes Here'>About 1</a></li>
+                        <li><a href='something.php' title='Title Goes Here'>About 2</a></li>
+                        <li><a href='something.php' title='Title Goes Here'>About 3</a></li>
+                        <li><a href='something.php' title='Title Goes Here'>About 4</a></li>
+                    </ul>
+                </li>
+                <li class='connect-tab'><a href='connect.php' title='Connect'>Connect</a>
+                    <ul class='connect dropdown-content'>
+                        <li><a href='something.php' title='Title Goes Here'>Content 1</a></li>
+                        <li><a href='something.php' title='Title Goes Here'>Content 2</a></li>
+                    </ul>
+                </li>
+                <li class='community-tab'><a href='community.php' title='Community'>Community</a>
+                    <ul class='community dropdown-content'>
+                        <li><a href='something.php' title='Title Goes Here'>Content 1</a></li>
+                        <li><a href='something.php' title='Title Goes Here'>Content 2</a></li>
+                        <li><a href='something.php' title='Title Goes Here'>Content 3</a></li>
+                        <li><a href='something.php' title='Title Goes Here'>Content 4</a></li>
+                    </ul>
+                </li>
+                <li class='support-tab'><a href='support.php' title='Support Us'>Support Us</a>
+                    <ul class='support dropdown-content'>
+                        <li><a href='something.php' title='Title Goes Here'>Content 3</a></li>
+                        <li><a href='something.php' title='Title Goes Here'>Content 4</a></li>
+                    </ul>
+                </li>
+                <li><a href='login.php' title='Login'>Login</a> </li>
+            </ul>
+        </nav>
+    </header>";
+
+$footer = "   
+<footer>
+    <div class='container-fluid'>
+        <div class='row main-footer'>
+            <div class='col-xs-12 col-md-4'>
+                <div class='footer-text'>
+                    <h3>Location</h3>
+                    <p class='footer-subhead'>Rehearsals</p>
+                    <address><a href='https://goo.gl/maps/t8qsr49SpQfC7ELc8' target='_blank' title='Google Maps'>Epworth United Methodist Church <br> 1400 S Cooper St <br> Arlington, TX 76013</a></address><br>
+                    <p> Tuesdays 7:00PM - 9:00PM</p>
+                </div>
+            </div>
+            <div class='col-xs-12 col-md-4'>
+                <div class='sitemap'>
+                    <h3>Sitemap</h3>
+                    <div class='footer_links'>
+                        <a href='index.php' title='Home' class='links'>Home</a><br>
+                        <a href='about.php' title='About' class='links'>About</a><br>
+                        <a href='connect.php' title='Connect' class='links'>Connect</a><br>
+                        <a href='community.php' title='Community'class='links'>Community</a><br>
+                        <a href='support.php' title='Support Us' class='links'>Support Us</a><br>
+                        <a href='login.php' title='Login' class='links'>Login</a>
+                    </div>
+                </div>
+            </div>
+            <div class='col-xs-12 col-md-4'>
+                <div class=''>
+                    <h3>Contact</h3>
+
+                    <p class='footer-subhead'>Mailing Address</p>
+                    <address><a href='https://goo.gl/maps/t8qsr49SpQfC7ELc8' target='_blank' title='Google Maps' class='links'>PO Box 1522 <br> Arlington, TX 76004</a>
+                    </address>
+
+                    <p>(682) -233-3606</p>
+
+                    <p class='footer_text_center'><a href='mailto:username@domain.com' title='Webmaster's Email' target='_blank' class='links'>gtchorus@gmail.com</a></p>
+
+                    <ul class='social-media'>
+                        <li><a href='https://facebook.com' title='Facebook' target='_blank'><i class='fab fa-facebook'></i></a></li>
+                        <li><a href='https://twitter.com' title='Twitter' target='_blank'><i class='fab fa-twitter'></i></a></li>
+                        <li><a href='https://linkedin.com' title='Linked In' target='_blank'><i class='fab fa-linkedin'></i></a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class='row legal'>
+        <div class='col-sm-12'>
+            <p> Copyright &copy 2023 The Goodtimes Chorus. All rights reserved. </p>
+        </div>
+    </div>
+</footer>";
+
+
+?>
